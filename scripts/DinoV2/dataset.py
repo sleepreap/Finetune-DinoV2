1ac51e38f7c2df2bc0458298a1cb4470df90dd4e
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-04-22 14:29:52 +0800

    Update dataset.py

--- scripts/DinoV2/dataset.py
+++ scripts/DinoV2/dataset.py
@@ -4,7 +4,15 @@ from torch.utils.data import Dataset
 import numpy as np
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader 
-from transformers import AutoImageProcessor
+import albumentations as A
+
+ADE_MEAN = np.array([123.675, 116.280, 103.530]) / 255
+ADE_STD = np.array([58.395, 57.120, 57.375]) / 255
+
+normalization= A.Compose([
+    A.Resize(width=644, height=644),
+    A.Normalize(mean=ADE_MEAN, std=ADE_STD),
+])
 
 class ImageSegmentationDataset(Dataset):
     def __init__(self, images_dir, masks_dir, transform=None):
@@ -21,34 +29,38 @@ class ImageSegmentationDataset(Dataset):
         mask_path = os.path.join(self.masks_dir, self.filenames[idx] + '.png') 
         image = Image.open(img_path).convert("RGB")
         np_image=np.array(image)
-        # convert to C, H, W
-        np_image = np_image.transpose(2,0,1)
         mask = Image.open(mask_path) 
         np_mask=np.array(mask)
         np_mask[np_mask==255]=1
-      
-        return np_image, np_mask
-    
+
+        #Transformation for normalization
+        transformed = self.transform(image=np_image, mask=np_mask)
+        image, target = torch.tensor(transformed['image']), torch.LongTensor(transformed['mask'])
+        # convert to C, H, W
+        image = image.permute(2,0,1)
+       
+        return image, target, np_image, np_mask
+
+
 class SegmentationDataModule(pl.LightningDataModule):
     def __init__(self, dataset_dir, batch_size, num_workers):
         super().__init__()
         self.dataset_dir = dataset_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-small-ade-semantic")
     
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
             self.train_dataset = ImageSegmentationDataset(images_dir=os.path.join(self.dataset_dir, 'images', 'train'),
                                                           masks_dir=os.path.join(self.dataset_dir, 'labels', 'train'),
-                                                          transform=None) # Add your transforms here
+                                                          transform=normalization) # Add your transforms here
             self.val_dataset = ImageSegmentationDataset(images_dir=os.path.join(self.dataset_dir, 'images', 'val'),
                                                         masks_dir=os.path.join(self.dataset_dir, 'labels', 'val'),
-                                                        transform=None) # Add your transforms here
+                                                        transform=normalization) # Add your transforms here
         if stage == 'test' or stage is None:
             self.test_dataset = ImageSegmentationDataset(images_dir=os.path.join(self.dataset_dir, 'images', 'test'),
                                                          masks_dir=os.path.join(self.dataset_dir, 'labels', 'test'),
-                                                         transform=None) # Add your transforms here
+                                                         transform=normalization) # Add your transforms here
     
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, collate_fn=self.collate_fn)
@@ -59,17 +71,12 @@ class SegmentationDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, collate_fn=self.collate_fn)
         
-    def collate_fn(self,batch):
-      inputs = list(zip(*batch))
-      images=inputs[0]
-      segmentation_maps=inputs[1]
-      batch = self.processor(
-          images,
-          segmentation_maps=segmentation_maps,
-          size=(640,640),
-          return_tensors="pt",
-      )
-      batch["original_images"] = images
-      batch["original_segmentation_maps"] = segmentation_maps
-
-      return batch
+    def collate_fn(inputs):
+        batch = dict()
+        batch["pixel_values"] = torch.stack([i[0] for i in inputs], dim=0) 
+        batch["labels"] = torch.stack([i[1] for i in inputs], dim=0)
+        batch["original_images"] = [i[2] for i in inputs]
+        batch["original_segmentation_maps"] = [i[3] for i in inputs]
+    
+        return batch
+    
