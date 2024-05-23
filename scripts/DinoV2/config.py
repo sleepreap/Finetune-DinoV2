e7cab767d22f9ac970f0efadfc657a8f5b7ff1ff
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-05-23 11:24:10 +0800

    Update config.py

--- scripts/DinoV2/config.py
+++ scripts/DinoV2/config.py
@@ -5,14 +5,14 @@ import sys
 import os
 
 #Training hyperparmeters
-LEARNING_RATE=0.00005
-EPOCHS=100
+LEARNING_RATE=0.0001
+EPOCHS=180
 PRECISION="16-mixed"
-DEVICES=[1,2,3]
+DEVICES=[2,3]
 EARLY_STOPPING_CALLBACK = EarlyStopping(
     monitor="valLoss",
     min_delta=0.00,
-    patience=4,
+    patience=55,
     verbose=True,
     mode="min",
 )
@@ -22,13 +22,16 @@ CHECKPOINT_CALLBACK = ModelCheckpoint(save_top_k=1,
                                       save_on_train_epoch_end=True  # Ensure saving happens at the end of a training epoch
                                      )
 LOGGER = CSVLogger("outputs", name="lightning_logs_csv")
+#lr Scheduler
+FACTOR=0.1
+PATIENCE=30
 
 #Dataset
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 dataset_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'dataset')
 DATASET_DIR=dataset_path
 NUM_WORKERS=4
-BATCH_SIZE=1
+BATCH_SIZE=2
 ID2LABEL={
     0: 'Background',
     1: 'Metal Lines'
